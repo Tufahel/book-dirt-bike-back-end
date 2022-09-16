0e3d3b677fbe54a9557318388a828b944d514052
Tufahel <tufahel96@gmail.com>
2022-09-16 20:08:33 +0600

    updated the relations and schema

--- db/migrate/20220916134742_create_motorcycles.rb
+++ db/migrate/20220916134742_create_motorcycles.rb
@@ -1,6 +1,7 @@
 class CreateMotorcycles < ActiveRecord::Migration[7.0]
   def change
     create_table :motorcycles do |t|
+      t.references :user, null: false, foreign_key: true
       t.string :bike_name
       t.text :details
       t.string :image
