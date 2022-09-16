218bbc75c14ce05cc39d500fc9db3b27649d2231
Tufahel <tufahel96@gmail.com>
2022-09-16 21:29:12 +0600

    cancan ability and permit email for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
     protected
   
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
     end
   end
