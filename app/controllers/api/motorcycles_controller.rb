08577219f81a95b174e9332eaee841574e07583b
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-09-20 07:51:45 +0100

    Motorcycke controller

--- app/controllers/api/motorcycles_controller.rb
+++ app/controllers/api/motorcycles_controller.rb
@@ -1,11 +1,39 @@
 class Api::MotorcyclesController < ApplicationController
-  def index; end
+  def index
+    @motorcycles = Motorcycle.all
+    render json: @motorcycles
+  end
 
-  def show; end
+  def show
+    @motorcycle = Motorcycle.find(params[:id])
+    render json: @motorcycle
+  end
 
-  def new; end
+  def new
+    @user = current_user.id
+    @motorcycles = @user.motorcycles
+    render json: @motorcycles
+  end
 
-  def create; end
+  def create
+    @motorcycle = Motorcycle.new(motorcycle_params)
+    @motorcycle.user_id = current_user.id
+    if @motorcycle.save
+      render json: @motorcycle
+    else
+      render json: @motorcycle.errors.full_messages, status: 422
+    end
+  end
 
-  def params; end
+  def destroy
+    @motorcycle = Motorcycle.find(params[:id])
+    @motorcycle.destroy
+    render json: @motorcycle
+  end
+
+  private
+
+  def motorcycle_params
+    params.require(:motorcycle).permit(:bike_name, :details, :image, :user_id)
+  end
 end
