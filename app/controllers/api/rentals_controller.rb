510158c135a85ece56381005adeee1097284c38e
Nii Amanor Djoleto <djoletoamanor@gmail.com>
2022-09-20 10:46:53 +0000

    Fix linters

--- app/controllers/api/rentals_controller.rb
+++ app/controllers/api/rentals_controller.rb
@@ -1,12 +1,12 @@
 class Api::RentalsController < ApplicationController
   def index
-  @rentals = Rental.all
-  render json: @rentals
+    @rentals = Rental.all
+    render json: @rentals
   end
 
   def show
-  @rental = Rental.find(params[:id])
-  render json: @rental
+    @rental = Rental.find(params[:id])
+    render json: @rental
   end
 
   def create; end
@@ -17,12 +17,9 @@ class Api::RentalsController < ApplicationController
     render json: @rentals
   end
 
-  def destroy
-    
-  end
+  def destroy; end
 
   def params
     params.require(:rentals).permit(:user_id, :motorcycle_id, :book_date, :return_date, :city)
   end
-  
 end
