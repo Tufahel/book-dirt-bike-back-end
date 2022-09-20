a8cdf22fb3d6ec22e9b8967b68e6bc08366e32cb
Nii Amanor Djoleto <djoletoamanor@gmail.com>
2022-09-20 10:36:42 +0000

    Added rental controller

--- app/controllers/api/rentals_controller.rb
+++ app/controllers/api/rentals_controller.rb
@@ -1,11 +1,21 @@
 class Api::RentalsController < ApplicationController
-  def index; end
+  def index
+  @rentals = Rental.all
+  render json: @rentals
+  end
 
-  def show; end
+  def show
+  @rental = Rental.find(params[:id])
+  render json: @rental
+  end
 
   def create; end
 
-  def new; end
+  def new
+    @user = current_user.id
+    @rentals = @user.rentals
+    render json: @rentals
+  end
 
   def destroy; end
 
